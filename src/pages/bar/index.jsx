f7abc1e08880e331bc66110f78aa034aa2c9e152
Tamir Atia <tamir111994@gmail.com>
2022-12-06 17:23:32 +0200

    add StatBox, ProgresCircle and DashboardGrid components

--- src/pages/bar/index.jsx
+++ src/pages/bar/index.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Box } from "@mui/material";
+import React from "react";
+import BarChart from "../../components/BarChart";
+import Header from "../../components/Header";
 
 const Bar = () => {
   return (
-    <div>Bar</div>
-  )
-}
+    <Box m="20px">
+      <Header title="BAR CHART" subtitle="Simple Bar Chart" />
+      <Box height="75vh">
+        <BarChart />
+      </Box>
+    </Box>
+  );
+};
 
-export default Bar
+export default Bar;
--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,13 +1,36 @@
-import { Box } from "@mui/material";
-import React from "react";
+import { Box, Button, useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+
+import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+
 import Header from "../../components/Header";
+import DashboardGrid from "../../components/DashboardGrid";
 
 const Dashboard = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
   return (
     <Box m="20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+        <Header title="DASHBOARD" subtitle="Welcome to your Dashboard" />
+
+        <Box>
+          <Button
+            sx={{
+              backgroundColor: colors.blueAccent[700],
+              color: colors.grey[100],
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "10px 20px",
+            }}
+          >
+            <DownloadOutlinedIcon sx={{ mr: "10px" }} />
+            Download Reports
+          </Button>
+        </Box>
       </Box>
+      <DashboardGrid />
     </Box>
   );
 };
